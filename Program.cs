63c1a47b3895944e1ae4bad73935c2414ad4be37
Act3Sun <act3jo@gmail.com>
2025-03-15 22:34:55 +0900

    first commit

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+using System.Threading;
+using System.Runtime;
+
+namespace Hello
+{
+    class H1
+    {
+        static void Main()
+        {
+            Console.WriteLine("HelloWorld");
+            int currentTop = Console.CursorTop;
+            int L =0;
+            int T =0;
+            for(int i=0; i<=100; i++)
+            {
+                Console.SetCursorPosition(0, currentTop);
+                Console.WriteLine($"{i}%");
+                T = Console.CursorTop;
+
+                Console.SetCursorPosition(L, T);
+                if(i%5 == 0 && i!=0)
+                {
+                    Console.Write("#");
+                }
+                L = Console.CursorLeft;
+                
+                Thread.Sleep(50);
+            }
+            Console.ReadKey();
+        }
+
+    }
+}
